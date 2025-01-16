aaf7edd7df2e0aab9aee4dce3537258f939184cb
Daniel Medina <paginamedina@gmail.com>
2025-01-16 16:38:32 -0500

    [DEV] - fixing footer spaces

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <footer className="mt-6 text-gray-200 text-sm relative flex flex-col justify-center border-t border-cyan-400 border-opacity-60">
-      <p className="text-center pt-4">
+      <p className="text-center pt-4 m-2">
         “El éxito no se logra solo con cualidades especiales. Es sobre todo un
         trabajo de mentalidad y actitud positiva. Sigue soñando, sigue
         creyendo.”
       </p>
-      <span className="font-bold text-cyan-500 pt-2 ml-2 mb-2">
+      <span className="font-bold text-cyan-500 pt-2 ml-4 mb-4">
         <a href="https://github.com/kiskee">DanMeCoss</a>
       </span>
     </footer>
