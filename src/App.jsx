efd14d3daf091c39b1e2af4bbdacb4a695d527cb
Daniel Medina <paginamedina@gmail.com>
2025-01-19 11:33:32 -0500

    [DEV] - adding analytics from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // For managing client-side routing
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/Home";
 import Layaut from "./components/Layaut";
-import Info from './pages/Info'
-import Register from './pages/Register'
+import Info from "./pages/Info";
+import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MyCourses from "./pages/MyCourses";
 import Courses from "./pages/Courses";
 
-
 export default function App() {
   return (
     <Router>
-        <Layaut>
-          <SpeedInsights />
-          <Routes>
-            {/* Define the route for the home page */}
-            <Route path="/" element={<Home />} />
-            {/* Define the route for the "Nosotros" page */}
-            <Route path="/info" element={<Info />} />
-            {/* Define the route for the "Proyectos" page */}
-            <Route path="/register" element={<Register />} />
+      <Layaut>
+        <Analytics />
+        <SpeedInsights />
+        <Routes>
+          {/* Define the route for the home page */}
+          <Route path="/" element={<Home />} />
+          {/* Define the route for the "Nosotros" page */}
+          <Route path="/info" element={<Info />} />
+          {/* Define the route for the "Proyectos" page */}
+          <Route path="/register" element={<Register />} />
 
-            <Route path="/courses" element={<Courses />} />
+          <Route path="/courses" element={<Courses />} />
 
-            <Route path="/my-courses" element={<ProtectedRoute element={<MyCourses/>} />} />
-          </Routes>
-        </Layaut>
-     
+          <Route
+            path="/my-courses"
+            element={<ProtectedRoute element={<MyCourses />} />}
+          />
+        </Routes>
+      </Layaut>
     </Router>
   );
 }
