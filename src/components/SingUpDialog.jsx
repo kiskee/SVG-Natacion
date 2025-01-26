8ad84117cbccf080e4e2f5008bb0c4b603c86975
Daniel Medina <paginamedina@gmail.com>
2025-01-26 16:07:47 -0500

    [DEV] - adding loader to the singup

--- src/components/SingUpDialog.jsx
+++ src/components/SingUpDialog.jsx
@@ -61,6 +61,7 @@ export default function SingUpDialog() {
   const [openSingINDialog, setOpenSingINDialog] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   // 1. Define your form.
   const form = useForm({
@@ -76,6 +77,7 @@ export default function SingUpDialog() {
 
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
+      setIsLoading(true);
       try {
         const userInfo = await axios.get(
           import.meta.env.VITE_GOOGLE_SINGIN_URL,
@@ -94,9 +96,10 @@ export default function SingUpDialog() {
           ...login.user,
           token: login.accessToken, // Suponiendo que tu API devuelve el token como `jwtToken`
         });
+        setIsLoading(false);
         navigate("/", { replace: true });
       } catch (error) {
-        setError(error)
+        setError(error);
       }
     },
     onError: (errorResponse) => console.log(errorResponse),
@@ -104,6 +107,7 @@ export default function SingUpDialog() {
 
   async function onSubmit(values) {
     try {
+      setIsLoading(true);
       const user = {
         email: values.email,
         family_name: values.apellidos,
@@ -121,9 +125,10 @@ export default function SingUpDialog() {
         ...login.user,
         token: login.accessToken, // Suponiendo que tu API devuelve el token como `jwtToken`
       });
+      setIsLoading(false);
       navigate("/", { replace: true });
     } catch (error) {
-      setError(error)
+      setError(error);
     }
   }
 
@@ -160,191 +165,214 @@ export default function SingUpDialog() {
                   className="relative z-10 mt-2"
                 />
               </div>
-             
+
               {error ? (
                 <h1 className="text-red-500 text-xl font-bold mt-8 text-center">
                   {error}
                 </h1>
               ) : (
                 <div className="mt-2 text-center text-xl text-white">
-                Para usar SVG - Natacion deberas ingresar en una cuenta
-                existente o crear una nueva
-              </div>
+                  Para usar SVG - Natacion deberas ingresar en una cuenta
+                  existente o crear una nueva
+                </div>
               )}
             </div>
 
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-                <div className="grid gap-4 py-4">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem
-                        className="grid grid-cols-4 items-center gap-4 mr-4"
-                        id="email-form-item"
-                      >
-                        <FormLabel className="text-right">Email</FormLabel>
-                        <div className="col-span-3">
-                          <FormControl>
-                            <Input
-                              placeholder="peduarte@me.com"
-                              type="email"
-                              className="w-2/3"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="nombre"
-                    render={({ field }) => (
-                      <FormItem
-                        className="grid grid-cols-4 items-center gap-4 mr-4"
-                        id="nombre-form-item"
-                      >
-                        <FormLabel className="text-right">Nombre</FormLabel>
-                        <div className="col-span-3">
-                          <FormControl>
-                            <Input
-                              placeholder="Pedro"
-                              className="w-2/3"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="apellidos"
-                    render={({ field }) => (
-                      <FormItem
-                        className="grid grid-cols-4 items-center gap-4 mr-4"
-                        id="apellidos-form-item"
-                      >
-                        <FormLabel className="text-right">Apellidos</FormLabel>
-                        <div className="col-span-3">
-                          <FormControl>
-                            <Input
-                              placeholder="Duarte"
-                              className="w-2/3"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="usuario"
-                    render={({ field }) => (
-                      <FormItem
-                        className="grid grid-cols-4 items-center gap-4 mr-4"
-                        id="usuario-form-item"
-                      >
-                        <FormLabel className="text-right">Usuario</FormLabel>
-                        <div className="col-span-3">
-                          <FormControl>
-                            <Input
-                              placeholder="peduarte"
-                              className="w-2/3"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem
-                        className="grid grid-cols-4 items-center gap-4 mr-4"
-                        id="password-form-item"
-                      >
-                        <FormLabel className="text-right">Contraseña</FormLabel>
-                        <div className="col-span-3">
-                          <div className="flex-row flex">
-                            <FormControl>
-                              <Input
-                                type={showPassword ? "text" : "password"}
-                                className="w-2/3"
-                                placeholder="Hello@123"
-                                {...field}
-                              />
-                            </FormControl>
-                            <button
-                              type="button"
-                              onClick={togglePasswordVisibility}
-                              className="ml-4"
-                              aria-label={
-                                showPassword
-                                  ? "Ocultar contraseña"
-                                  : "Mostrar contraseña"
-                              }
-                            >
-                              {showPassword ? (
-                                <EyeOff className="w-5 h-5 text-gray-500" />
-                              ) : (
-                                <Eye className="w-5 h-5 text-gray-500" />
-                              )}
-                            </button>
-                          </div>
-                          <FormDescription className="text-xs mt-2">
-                            Debe incluir mayúsculas, minúsculas, números y
-                            caracteres especiales (@$!%*?&)
-                          </FormDescription>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
+            {isLoading ? (
+              <div className="text-center mb-4">
+                <h1 className="text-4xl font-bold text-cyan-500 mb-4 animate-pulse">
+                  Cargando tu perfil, gracias por tu paciencia...
+                </h1>
+                <div className="flex justify-center">
+                  {/* Spinner animado con Tailwind */}
+                  <div className="w-16 h-16 border-4 border-cyan-500 border-t-transparent rounded-full animate-spin"></div>
                 </div>
-                <div className="flex items-center flex-col gap-4">
-                  <Button
-                    type="submit"
-                    className="bg-cyan-500 text-black text-xl w-2/3"
+              </div>
+            ) : (
+              <>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-full"
                   >
-                    Registrate
-                  </Button>
-                </div>
-              </form>
-            </Form>
-            <Button
-              className="bg-white text-black text-xl w-2/3 "
-              onClick={googleLogin}
-            >
-              Registrate Con Google
-            </Button>
+                    <div className="grid gap-4 py-4">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem
+                            className="grid grid-cols-4 items-center gap-4 mr-4"
+                            id="email-form-item"
+                          >
+                            <FormLabel className="text-right">Email</FormLabel>
+                            <div className="col-span-3">
+                              <FormControl>
+                                <Input
+                                  placeholder="peduarte@me.com"
+                                  type="email"
+                                  className="w-2/3"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
 
-            <Button
-              onClick={() => setOpenSingINDialog(true)}
-              className="bg-yellow-500 text-black text-xl w-2/3 "
-            >
-              Inicia Session
-            </Button>
-            <p className="text-xs">
-              Al utilizar SVG - Natacion, acepta la recopilación de datos de uso
-              para análisis
-            </p>
-            <SingInDialog
-              openDialog={openSingINDialog}
-              closeDialog={(v) => setOpenSingINDialog(v)}
-            />
+                      <FormField
+                        control={form.control}
+                        name="nombre"
+                        render={({ field }) => (
+                          <FormItem
+                            className="grid grid-cols-4 items-center gap-4 mr-4"
+                            id="nombre-form-item"
+                          >
+                            <FormLabel className="text-right">Nombre</FormLabel>
+                            <div className="col-span-3">
+                              <FormControl>
+                                <Input
+                                  placeholder="Pedro"
+                                  className="w-2/3"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
+
+                      <FormField
+                        control={form.control}
+                        name="apellidos"
+                        render={({ field }) => (
+                          <FormItem
+                            className="grid grid-cols-4 items-center gap-4 mr-4"
+                            id="apellidos-form-item"
+                          >
+                            <FormLabel className="text-right">
+                              Apellidos
+                            </FormLabel>
+                            <div className="col-span-3">
+                              <FormControl>
+                                <Input
+                                  placeholder="Duarte"
+                                  className="w-2/3"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
+
+                      <FormField
+                        control={form.control}
+                        name="usuario"
+                        render={({ field }) => (
+                          <FormItem
+                            className="grid grid-cols-4 items-center gap-4 mr-4"
+                            id="usuario-form-item"
+                          >
+                            <FormLabel className="text-right">
+                              Usuario
+                            </FormLabel>
+                            <div className="col-span-3">
+                              <FormControl>
+                                <Input
+                                  placeholder="peduarte"
+                                  className="w-2/3"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem
+                            className="grid grid-cols-4 items-center gap-4 mr-4"
+                            id="password-form-item"
+                          >
+                            <FormLabel className="text-right">
+                              Contraseña
+                            </FormLabel>
+                            <div className="col-span-3">
+                              <div className="flex-row flex">
+                                <FormControl>
+                                  <Input
+                                    type={showPassword ? "text" : "password"}
+                                    className="w-2/3"
+                                    placeholder="Hello@123"
+                                    {...field}
+                                  />
+                                </FormControl>
+                                <button
+                                  type="button"
+                                  onClick={togglePasswordVisibility}
+                                  className="ml-4"
+                                  aria-label={
+                                    showPassword
+                                      ? "Ocultar contraseña"
+                                      : "Mostrar contraseña"
+                                  }
+                                >
+                                  {showPassword ? (
+                                    <EyeOff className="w-5 h-5 text-gray-500" />
+                                  ) : (
+                                    <Eye className="w-5 h-5 text-gray-500" />
+                                  )}
+                                </button>
+                              </div>
+                              <FormDescription className="text-xs mt-2">
+                                Debe incluir mayúsculas, minúsculas, números y
+                                caracteres especiales (@$!%*?&)
+                              </FormDescription>
+                              <FormMessage />
+                            </div>
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <div className="flex items-center flex-col gap-4">
+                      <Button
+                        type="submit"
+                        className="bg-cyan-500 text-black text-xl w-2/3"
+                      >
+                        Registrate
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+                <Button
+                  className="bg-white text-black text-xl w-2/3 "
+                  onClick={googleLogin}
+                >
+                  Registrate Con Google
+                </Button>
+
+                <Button
+                  onClick={() => setOpenSingINDialog(true)}
+                  className="bg-yellow-500 text-black text-xl w-2/3 "
+                >
+                  Inicia Session
+                </Button>
+                <p className="text-xs">
+                  Al utilizar SVG - Natacion, acepta la recopilación de datos de
+                  uso para análisis
+                </p>
+                <SingInDialog
+                  openDialog={openSingINDialog}
+                  closeDialog={(v) => setOpenSingINDialog(v)}
+                />
+              </>
+            )}
           </div>
         </>
       )}
